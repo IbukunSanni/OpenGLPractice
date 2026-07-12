a62db2b760154fd0532c6d288d36cf571a93a633
AzureAD\IbukunSanni <isanni@wsi-tech.com>
2026-07-12 08:52:04 -0400

    fix: sync EBO.h constructor signature with EBO.cpp vector overload

--- projects/12_mesh/EBO.h
+++ projects/12_mesh/EBO.h
@@ -1,15 +1,15 @@
-#ifndef EBO_CLASS_H
-#define EBO_CLASS_H
+#pragma once
 
-#include<glad/glad.h>
+#include <glad/glad.h>
+#include <vector>
 
 class EBO
 {
 public:
 	// ID reference of Elements Buffer Object
 	GLuint ID;
-	// Constructor that generates a Elements Buffer Object and links it to indices
-	EBO(GLuint* indices, GLsizeiptr size);
+	// Constructor that generates an Element Buffer Object and uploads indices to the GPU
+	EBO(std::vector<GLuint>& indices);
 
 	// Binds the EBO
 	void Bind();
@@ -18,5 +18,3 @@ public:
 	// Deletes the EBO
 	void Delete();
 };
-
-#endif
